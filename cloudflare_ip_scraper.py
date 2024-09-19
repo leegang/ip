450a694c5c52dfbb9e24a29707e36ac4011d22d5
leegang <lee_gang@126.com>
2024-09-19 23:53:23 +0800

    Update cloudflare_ip_scraper.py

--- cloudflare_ip_scraper.py
+++ cloudflare_ip_scraper.py
@@ -6,7 +6,6 @@ import ipaddress
 
 def fetch_and_parse(url):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         'Cache-Control': 'no-cache'
     }
     response = requests.get(url, headers=headers)
